cbca815839d0d1f80343d8a23a7a89db08513ec5
geoffrey <ckevines@naver.com>
2025-02-19 21:28:58 +0900

    Instead of using @MapperScan, you might want to try configuring the MapperScannerConfigurer explicitly in a configuration class

--- src/main/java/com/example/mybatis_graalvm/MybatisGraalvmApplication.java
+++ src/main/java/com/example/mybatis_graalvm/MybatisGraalvmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.mybatis_graalvm.mapper")
 public class MybatisGraalvmApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/mybatis_graalvm/config/MyBatisConfig.java
@@ -0,0 +1,15 @@
+package com.example.mybatis_graalvm.config;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setBasePackage("com.example.mybatis_graalvm.mapper");
+        return scannerConfigurer;
+    }
+}
